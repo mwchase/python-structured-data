fe3ebc59bf2fe2928a218a02fdd5f063bdb9c99a
Max Chase <max.chase@gmail.com>
2018-06-17 17:17:46 -0400

    Don't bother to partially support 3.6.5 if I can't figure out how to enmatrix it

--- tests/test_structured_data.py
+++ tests/test_structured_data.py
@@ -1,7 +1,4 @@
-import sys
 import typing
-
-import pytest
 
 T = typing.TypeVar('T')
 
@@ -10,26 +7,11 @@ def test_main():
     pass
 
 
-@pytest.mark.skipif(
-    sys.version_info >= (3, 6, 6),
-    reason='Test fails on newer Pythons')
-def test_generic_subclass_fails():
-    import structured_data
-
-    with pytest.raises(RuntimeError):
-        @structured_data.enum
-        class TestClass(typing.Generic[T]):
-            Variant: structured_data.Ctor[()]
-
-
-@pytest.mark.skipif(
-    sys.version_info < (3, 6, 6),
-    reason='Test fails on older Pythons')
 def test_generic_subclass_succeeds():
     import structured_data
 
     @structured_data.enum
     class TestClass(typing.Generic[T]):
-        Variant: structured_data.Ctor[()]
+        pass
 
     assert TestClass.Variant()
