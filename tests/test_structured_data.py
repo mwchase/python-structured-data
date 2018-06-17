0e20662ba4244df249ea6408d89d5b793754e72f
Max Chase <max.chase@gmail.com>
2018-06-17 17:04:25 -0400

    Test the Generic bug fixed in 3.6.6

--- tests/test_structured_data.py
+++ tests/test_structured_data.py
@@ -1,2 +1,35 @@
+import sys
+import typing
+
+import pytest
+
+T = typing.TypeVar('T')
+
+
 def test_main():
     pass
+
+
+@pytest.mark.skipif(
+    sys.version_info >= (3, 6, 6),
+    reason='Test fails on newer Pythons')
+def test_generic_subclass_fails():
+    import structured_data
+
+    with pytest.raises(RuntimeError):
+        @structured_data.enum
+        class TestClass(typing.Generic[T]):
+            Variant: structured_data.Ctor[()]
+
+
+@pytest.mark.skipif(
+    sys.version_info < (3, 6, 6),
+    reason='Test fails on older Pythons')
+def test_generic_subclass_succeeds():
+    import structured_data
+
+    @structured_data.enum
+    class TestClass(typing.Generic[T]):
+        Variant: structured_data.Ctor[()]
+
+    assert TestClass.Variant()
