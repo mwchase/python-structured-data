b8b908b0df6a32cea8590781cb961c3160151585
Max Chase <max.chase@gmail.com>
2018-06-21 22:10:51 -0400

    Test the matching code

--- tests/test_structured_data.py
+++ tests/test_structured_data.py
@@ -35,3 +35,21 @@ def test_ctor_controls_subclass_creation(structured_data):
 def test_ctor_cant_index_twice(structured_data):
     with pytest.raises(TypeError):
         assert not structured_data.Ctor[list][list]
+
+
+def test_matching(structured_data):
+
+    @structured_data.enum
+    class TestClass:
+        StrPair: structured_data.Ctor[str, str]
+    matcher = structured_data.ValueMatcher(
+        ((1, 2), TestClass.StrPair('a', 'b')))
+    assert not matcher.match((
+        (structured_data.Pattern('_'), 4),
+        structured_data.Pattern('_')))
+    assert matcher.matches is None
+    assert matcher.match((
+        structured_data.Pattern('tup') @ (1, structured_data.Pattern('a')),
+        TestClass.StrPair(
+            structured_data.Pattern('b'), structured_data.Pattern('c'))))
+    assert matcher.matches == dict(tup=(1, 2), a=2, b='a', c='b')
