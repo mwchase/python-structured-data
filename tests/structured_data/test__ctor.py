e02a91efeb9927a0e7bd3f5499a9420e49b9e893
Max Chase <max.chase@gmail.com>
2018-07-19 15:56:42 -0400

    Move Ctor tests to matching test module

---
+++ tests/structured_data/test__ctor.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+def test_ctor_usable_as_empty(enum):
+    assert enum.Ctor is enum.Ctor[()]
+
+
+def test_ctor_converts_to_tuple(enum):
+    assert enum.Ctor[(list,)] is enum.Ctor[list]
+
+
+def test_ctor_controls_subclass_creation(enum):
+    with pytest.raises(TypeError):
+        class CantMake(enum.Ctor):
+            pass
+
+
+def test_ctor_cant_index_twice(enum):
+    with pytest.raises(TypeError):
+        assert not enum.Ctor[list][list]
--- tests/structured_data/test_enum.py
+++ tests/structured_data/test_enum.py
@@ -23,25 +23,6 @@ def test_generic_subclass_succeeds(enum):
     assert TestClass.Variant()
 
 
-def test_ctor_usable_as_empty(enum):
-    assert enum.Ctor is enum.Ctor[()]
-
-
-def test_ctor_converts_to_tuple(enum):
-    assert enum.Ctor[(list,)] is enum.Ctor[list]
-
-
-def test_ctor_controls_subclass_creation(enum):
-    with pytest.raises(TypeError):
-        class CantMake(enum.Ctor):
-            pass
-
-
-def test_ctor_cant_index_twice(enum):
-    with pytest.raises(TypeError):
-        assert not enum.Ctor[list][list]
-
-
 def test_enum_class(enum_module):
     for enum_class in enum_module.TEST_CLASSES:
         assert enum_class
