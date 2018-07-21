94f89fb83a8b800154e1f154ea0084f52b7fb1d3
Max Chase <max.chase@gmail.com>
2018-07-21 17:56:20 -0400

    Move processors to own module. There's a bug

---
+++ src/structured_data/_processors.py
@@ -0,0 +1,51 @@
+from ._enum_constructor import EnumConstructor
+from ._match_failure import MatchFailure
+from ._patterns import AsPattern
+from ._unpack import unpack
+
+
+def as_pattern_processor(target):
+    def processor(value):
+        if target is value:
+            yield target.match
+            yield target.matcher
+        else:
+            yield value
+            yield value
+    return processor
+
+
+def enum_processor(target):
+    def processor(value):
+        if value.__class__ is not target.__class__:
+            raise MatchFailure
+        yield from reversed(unpack(value))
+    return processor
+
+
+def tuple_processor(target):
+    def processor(value):
+        if isinstance(value, target.__class__) and len(target) == len(value):
+            yield from reversed(value)
+        else:
+            raise MatchFailure
+    return processor
+
+
+class ProcessorList:
+
+    def __init__(self, processors=()):
+        self.processors = list(processors)
+
+    def get_processor(self, item):
+        for typ, meta_processor in self.processors:
+            if isinstance(item, typ):
+                return meta_processor(item)
+        return None
+
+
+PROCESSORS = ProcessorList((
+    (AsPattern, as_pattern_processor),
+    (EnumConstructor, enum_processor),
+    (tuple, tuple_processor),
+))
---
+++ tests/structured_data/test__processors.py
@@ -0,0 +1,29 @@
+def test_tuple(match):
+
+    matcher = match.ValueMatcher((1, 2))
+    assert not matcher.match((3,))
+    assert matcher.matches is None
+    structure = (1, match.pat.a)
+    assert matcher.match(structure)
+
+
+def test_enum(enum, match):
+
+    @enum.enum
+    class TestClass:
+        StrPair: enum.Ctor[str, str]
+        Str: enum.Ctor[str]
+    matcher = match.ValueMatcher(TestClass.StrPair('a', 'b'))
+    assert not matcher.match(TestClass.Str('c'))
+    assert matcher.matches is None
+    structure = TestClass.StrPair(match.pat.a, match.pat.b)
+    assert matcher.match(structure)
+
+
+def test_as(match):
+
+    matcher = match.ValueMatcher((1, 2))
+    assert not matcher.match(match.pat.tup @ (match.pat.a, 3))
+    assert matcher.matches is None
+    structure = match.pat.tup @ (1, match.pat.a)
+    assert matcher.match(structure)
