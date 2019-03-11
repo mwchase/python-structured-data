a98727e5c7c3ce31bfdb104520b4f941a7ccf143
Max Chase <max.chase@gmail.com>
2019-03-11 13:37:01 -0400

    Activate imgconverter extension

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
+    "sphinx.ext.imgconverter",
 ]
 if os.getenv("SPELLCHECK"):
     extensions += ("sphinxcontrib.spelling",)
