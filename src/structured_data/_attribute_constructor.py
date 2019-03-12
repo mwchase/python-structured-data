aeca65cdac5949d46de6333de298f0c183c13d34
Max Chase <max.chase@gmail.com>
2019-03-12 15:54:41 -0400

    Add annotations

--- src/structured_data/_attribute_constructor.py
+++ src/structured_data/_attribute_constructor.py
@@ -1,19 +1,22 @@
 import sys
+import typing
 import weakref
 
-ATTRIBUTE_CONSTRUCTORS = weakref.WeakKeyDictionary()
-ATTRIBUTE_CACHE = weakref.WeakKeyDictionary()
+T = typing.TypeVar("T")
+
+ATTRIBUTE_CONSTRUCTORS: typing.MutableMapping = weakref.WeakKeyDictionary()
+ATTRIBUTE_CACHE: typing.MutableMapping = weakref.WeakKeyDictionary()
 
 
-class AttributeConstructor:
+class AttributeConstructor(typing.Generic[T]):
 
     __slots__ = ("__weakref__",)
 
-    def __init__(self, constructor):
+    def __init__(self, constructor: typing.Type[T]):
         ATTRIBUTE_CONSTRUCTORS[self] = constructor
         ATTRIBUTE_CACHE[self] = {}
 
-    def __getattribute__(self, name):
+    def __getattribute__(self, name: str) -> T:
         name = sys.intern(name)
         return ATTRIBUTE_CACHE[self].setdefault(
             name, ATTRIBUTE_CONSTRUCTORS[self](name)
