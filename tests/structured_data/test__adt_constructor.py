0d575204c510ad1316e06c56f78b19d7c1d6abf2
Max Chase <max.chase@gmail.com>
2019-03-12 21:38:25 -0400

    Add test for dir overrides

--- tests/structured_data/test__adt_constructor.py
+++ tests/structured_data/test__adt_constructor.py
@@ -20,3 +20,15 @@ def test_cant_pass_extra(option_class):
 def test_cant_pass_nothing(option_class):
     with pytest.raises(ValueError):
         assert not option_class.Left()
+
+
+def test_non_existent_dir_entry_is_acceptable(adt):
+    @adt.adt
+    class TestDir:
+        Int: adt.Ctor[int]
+        Str: adt.Ctor[str]
+
+        def __dir__(self):
+            return super().__dir__() + ["123fake"]
+
+    assert "123fake" in dir(TestDir.Int(5))
