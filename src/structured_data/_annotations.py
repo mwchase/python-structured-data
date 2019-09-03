bbe4b54227d9b910ae9608aa0e8534a28bd652dc
Max Chase <max.chase@gmail.com>
2019-09-02 22:39:36 -0400

    Conciseness improvement

--- src/structured_data/_annotations.py
+++ src/structured_data/_annotations.py
@@ -6,18 +6,14 @@ import typing
 from . import _ctor
 from . import _nillable_write
 
-_T = typing.TypeVar("_T")
 
-
-def _all_annotations(
-    cls: typing.Type[_T]
-) -> typing.Iterator[typing.Tuple[typing.Type[_T], str, typing.Any]]:
+def _all_annotations(cls: type) -> typing.Iterator[typing.Tuple[type, str, typing.Any]]:
     for superclass in reversed(cls.__mro__):
         for key, value in vars(superclass).get("__annotations__", {}).items():
             yield (superclass, key, value)
 
 
-def sum_args_from_annotations(cls: typing.Type[_T]) -> typing.Dict[str, typing.Tuple]:
+def sum_args_from_annotations(cls: type) -> typing.Dict[str, typing.Tuple]:
     """Return the constructor data for Sum classes."""
     args: typing.Dict[str, typing.Tuple] = {}
     for superclass, key, value in _all_annotations(cls):
@@ -27,9 +23,7 @@ def sum_args_from_annotations(cls: typing.Type[_T]) -> typing.Dict[str, typing.T
     return args
 
 
-def product_args_from_annotations(
-    cls: typing.Type[_T]
-) -> typing.Dict[str, typing.Any]:
+def product_args_from_annotations(cls: type) -> typing.Dict[str, typing.Any]:
     """Return the field data for Product classes."""
     args: typing.Dict[str, typing.Any] = {}
     for superclass, key, value in _all_annotations(cls):
