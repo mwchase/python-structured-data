d7aedc9adcb106b0db56265478a91740a4fda283
Max Chase <max.chase@gmail.com>
2019-09-16 13:15:16 -0400

    Get rename logic right

--- mutmut_helper.py
+++ mutmut_helper.py
@@ -23,13 +23,12 @@ def cache_files(file_list):
     for file_ in file_list:
         dirname, fn = os.path.split(file_)
         cache_dir = os.path.join(dirname, "__pycache__")
-        prefix = fn + os.extsep
         try:
             cache_files = os.listdir(cache_dir)
         except FileNotFoundError:
             continue
         for cache_file in cache_files:
-            if cache_file.startswith(prefix):
+            if cache_file.startswith(fn):
                 yield os.path.join(cache_dir, cache_file)
 
 
