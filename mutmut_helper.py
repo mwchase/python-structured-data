c4fafb5efba23b0f842c2fa683bccefb795676d9
Max Chase <max.chase@gmail.com>
2019-09-16 15:26:49 -0400

    Attempt to fix mutmut_helper

--- mutmut_helper.py
+++ mutmut_helper.py
@@ -2,21 +2,14 @@ import os.path
 import subprocess
 
 
-def untracked_files():
+def modified_files():
     for file_ in (
-        subprocess.run(["hg", "status", "--unknown"], capture_output=True)
+        subprocess.run(["hg", "status", "--modified"], capture_output=True)
         .stdout.decode()
         .split("\n")
     ):
         if file_:
             yield file_[2:]
-
-
-def backup_files(file_list):
-    for file_ in file_list:
-        fn, ext = os.path.splitext(file_)
-        if ext == ".bak":
-            yield fn
 
 
 def cache_files(file_list):
@@ -33,6 +26,6 @@ def cache_files(file_list):
 
 
 if __name__ == "__main__":
-    for cache_file in cache_files(backup_files(untracked_files())):
+    for cache_file in cache_files(modified_files()):
         print(cache_file)
         os.remove(cache_file)
