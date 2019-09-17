ac36c4ea22eef2eb7932a22f1b7b197b520b3163
Max Chase <max.chase@gmail.com>
2019-09-17 19:43:51 -0400

    Require some further correctness from the decorator

--- tests/structured_data/test__pep_570_when.py
+++ tests/structured_data/test__pep_570_when.py
@@ -1,3 +1,6 @@
+import inspect
+
+
 def test_no_collide(_pep_570_when):
     @_pep_570_when.pep_570_when
     def decorated(throwaway, kwargs):
@@ -8,3 +11,12 @@ def test_no_collide(_pep_570_when):
         "kwargs": True,
         "throwaway": False,
     }
+    assert decorated.__name__ == "decorated"
+
+    signature = inspect.signature(decorated)
+    assert signature.parameters["throwaway"] == inspect.Parameter(
+        "throwaway", inspect.Parameter.POSITIONAL_ONLY
+    )
+    assert signature.parameters["kwargs"] == inspect.Parameter(
+        "kwargs", inspect.Parameter.VAR_KEYWORD
+    )
