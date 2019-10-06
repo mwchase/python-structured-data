d920f89c95a59feaeb82690664379f3b283de80f
Max Chase <max.chase@gmail.com>
2019-10-06 12:28:21 -0400

    Test for the exact expected strings

--- tests/structured_data/test__cant_modify.py
+++ tests/structured_data/test__cant_modify.py
@@ -1,5 +1,8 @@
 import pytest
 
+NO_ATTRIBUTE = r"^{klass.__name__!r} object has no attribute {name!r}$"
+READ_ONLY = r"^{klass.__name__!r} object attribute {name!r} is read-only$"
+
 
 def test_sum_property(adt):
     class TestSum(adt.Sum):
@@ -16,15 +19,25 @@ def test_sum_property(adt):
     assert test_value.prop is None
     with pytest.raises(AttributeError):
         test_value.prop = 1
-    with pytest.raises(AttributeError, match=" object has no attribute "):
+    with pytest.raises(
+        AttributeError, match=NO_ATTRIBUTE.format(klass=TestSum.Left, name="dne")
+    ):
         test_value.dne = 1
-    with pytest.raises(AttributeError, match=" object attribute .* is read-only$"):
+    with pytest.raises(
+        AttributeError, match=READ_ONLY.format(klass=TestSum.Left, name="Left")
+    ):
         test_value.Left = 1
-    with pytest.raises(AttributeError, match=" object attribute .* is read-only$"):
+    with pytest.raises(
+        AttributeError, match=READ_ONLY.format(klass=TestSum.Left, name="none")
+    ):
         test_value.none = 1
     with pytest.raises(AttributeError):
         del test_value.prop
-    with pytest.raises(AttributeError, match=" object has no attribute "):
+    with pytest.raises(
+        AttributeError, match=NO_ATTRIBUTE.format(klass=TestSum.Left, name="dne")
+    ):
         del test_value.dne
-    with pytest.raises(AttributeError, match=" object attribute .* is read-only$"):
+    with pytest.raises(
+        AttributeError, match=READ_ONLY.format(klass=TestSum.Left, name="Left")
+    ):
         del test_value.Left
