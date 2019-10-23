f3ca91cb6cd84b0d3bc2816b6cbec00527e62476
Max Chase <max.chase@gmail.com>
2019-10-22 21:21:39 -0400

    Pull out helper function

--- src/structured_data/_match/descriptor/function.py
+++ src/structured_data/_match/descriptor/function.py
@@ -27,6 +27,24 @@ def _dispatch(func, matches, bound_args, bound_kwargs):
     return func(*function_args.args, **function_args.kwargs)
 
 
+def _bound_and_values(signature, args, kwargs):
+    # The signature lets us regularize the call and apply any defaults
+    bound_arguments = signature.bind(*args, **kwargs)
+    bound_arguments.apply_defaults()
+
+    # Extract the *args and **kwargs, if any.
+    # These are never used in the matching, just passed to the underlying function
+    bound_args = ()
+    bound_kwargs = {}
+    values = bound_arguments.arguments.copy()
+    for parameter in signature.parameters.values():
+        if parameter.kind is inspect.Parameter.VAR_POSITIONAL:
+            bound_args = values.pop(parameter.name)
+        if parameter.kind is inspect.Parameter.VAR_KEYWORD:
+            bound_kwargs = values.pop(parameter.name)
+    return bound_args, bound_kwargs, values
+
+
 class Function(common.Descriptor):
     """Decorator with value-based dispatch. Acts as a function."""
 
@@ -37,29 +55,12 @@ class Function(common.Descriptor):
         # further development.
         self.matchers: common.MatchTemplate[typing.Any] = common.MatchTemplate()
 
-    def _bound_and_values(self, args, kwargs):
-        # Then we figure out what signature we're giving the outside world.
-        signature = inspect.signature(self)
-        # The signature lets us regularize the call and apply any defaults
-        bound_arguments = signature.bind(*args, **kwargs)
-        bound_arguments.apply_defaults()
-
-        # Extract the *args and **kwargs, if any.
-        # These are never used in the matching, just passed to the underlying function
-        bound_args = ()
-        bound_kwargs = {}
-        values = bound_arguments.arguments.copy()
-        for parameter in signature.parameters.values():
-            if parameter.kind is inspect.Parameter.VAR_POSITIONAL:
-                bound_args = values.pop(parameter.name)
-            if parameter.kind is inspect.Parameter.VAR_KEYWORD:
-                bound_kwargs = values.pop(parameter.name)
-        return bound_args, bound_kwargs, values
-
     def __call__(self, /, *args, **kwargs):  # noqa: E225
         # Okay, so, this is a convoluted mess.
 
-        bound_args, bound_kwargs, values = self._bound_and_values(args, kwargs)
+        bound_args, bound_kwargs, values = _bound_and_values(
+            inspect.signature(self), args, kwargs
+        )
 
         instance = None
         if args:
